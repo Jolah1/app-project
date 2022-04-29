480ad7f4d5970fb3c306dfbce9ee654f88d13e71
okunlade Jolade <jokunlade@gmail.com>
2022-04-29 21:28:53 +0100

    adding more function

--- app.js
+++ app.js
@@ -38,3 +38,77 @@ toUpper(buyer);
 toUpper(day);
 toUpper(price);
 
+const myAge = 26;
+
+const yourAge = 20;
+
+console.log(myAge + yourAge);
+const age = 16;
+
+console.log("Hello my name is " + "Folarinwa");
+
+console.log('hello it\'s the manager');
+console.log(`hello its me ${name} and my age is ${age}`);
+
+
+if (age === 18) {
+    console.log("congratulations you are good to go!");
+} else if (age <= 15) {
+console.log("wow you are really young.....make your purchase next year");
+}else {
+    console.log("you are not old enough");
+}
+
+const dice1 = 6;
+const dice2 = 3;
+
+if (dice1 === 6 || dice2 === 6) {
+    console.log("you rolled a double");
+} else {
+    console.log("Game over");
+}
+
+const schedule = ['wake up', 'pray', 'post products', 'do adverts', 'take orders', 'delivery'];
+
+schedule.push('Hey a new thing got added');
+schedule.pop();
+schedule.shift('drink a lot of water');
+schedule.unshift('God first');
+const phoneNumbers = [08136676771, 07031563254, 08067543322];
+
+console.log(schedule);
+
+console.log(schedule[0]);
+
+console.log(phoneNumbers[2]);
+
+
+const user = {
+    name: 'Jolly',
+    age: 26,
+    married: false,
+    purchases: ["schoolshoes", "flat shoes", "sneakers", "slippers"]
+
+};
+
+console.log(user.purchases);
+console.log(user.age);
+
+const names = ['jola', 'ade', 'fola', 'sarah', 'queen', 'kay'];
+
+for(name of names ){
+    console.log(`Hello dear ${name}`);
+    if (name === "queen") {
+        console.log("Jola is buying a sneakers");
+        break;
+    }
+}
+
+
+let loading = 0;
+
+while (loading < 100) {
+    console.log("website on rebranding");
+
+    loading++;
+}
