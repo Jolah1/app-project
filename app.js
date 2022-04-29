0660fe33be14f6eede0f1ae8b55437d417e9bd52
okunlade Jolade <jokunlade@gmail.com>
2022-04-29 18:18:32 +0100

    more commits

---
+++ app.js
@@ -0,0 +1,40 @@
+console.log('Hello Fola kiddies store');
+
+let shoe = 100;
+shoe = shoe - 10;
+shoe = shoe * 20;
+let name = "Folarin"; 
+let checkout = true;
+let box;
+console.log(shoe); 
+
+const brand = "vans sneakers";
+const buyer = "pay before service";
+const day = "same day delivery";
+const price = "no pricing";
+
+function logger() {
+    console.log("school shoes");
+    console.log("school shoes");
+    console.log("school shoes");
+    console.log("school shoes");
+    console.log("school shoes");
+}
+
+logger();
+
+function adder(num1, num2) {
+    console.log(num1 + num2);
+}
+adder(5, 10);
+
+function toUpper(text) {
+    const upperCased = text.toUpperCase();
+    console.log(upperCased);
+}
+
+toUpper(brand);
+toUpper(buyer);
+toUpper(day);
+toUpper(price);
+
